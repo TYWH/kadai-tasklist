8f0d8a192da1b3509c720b8c61dfff968b437320
TYWT <47287957+TYWH@users.noreply.github.com>
2019-04-03 13:26:51 +0000

    AddTasklistsTable

--- database/migrations/2019_04_03_195635_add_tasklists_table.php
+++ database/migrations/2019_04_03_195635_add_tasklists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasklistsTable extends Migration
+class AddTasklistsTable extends Migration
 {
     /**
      * Run the migrations.
